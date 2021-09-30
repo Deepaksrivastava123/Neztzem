5b3b200cfedb9efee660e96dc0ebc731c2e3ed51
Shivam  Jaiswal <shivamcse0036@gmail.com>
2021-09-30 13:31:55 +0530

    Splash screen

---
+++ app/src/main/java/com/example/neztzem/Activities/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.example.neztzem.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.neztzem.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+}
--- app/src/main/java/com/example/neztzem/Activities/SelectLanguageActivity.java
+++ app/src/main/java/com/example/neztzem/Activities/SelectLanguageActivity.java
@@ -2,15 +2,37 @@ package com.example.neztzem.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.neztzem.R;
 
-public class SelectLanguageActivity extends AppCompatActivity {
+public class SelectLanguageActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_language);
+
+        handleClicks();
+    }
+
+    private void handleClicks() {
+        findViewById(R.id.button_english).setOnClickListener(this);
+        findViewById(R.id.button_hindi).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+     if (v.getId() == R.id.button_english){
+
+     }
+     else {
+
+     }
+       startActivity(new Intent(SelectLanguageActivity.this,RegisterActivity.class));
+       finish();
     }
 }
